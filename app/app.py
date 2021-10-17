3a6fa97c6fc848a4a75a51430a4061a629f2c6e1
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2021-10-16 22:15:35 -0500

    ESTRUCTURA PÁGINA
    Documentos html y css generados.

--- app/app.py
+++ app/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hola'
+    return render_template('index.html'); 
 
 if __name__ == '__main__':
     app.run(debug=True, port=6060)
